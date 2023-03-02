7dcdac7ab0f9142b1552366e5fd27d0c22d8d8b4
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-02 15:12:09 +0100

    api for shortended link

--- src/router/LinkRouter.ts
+++ src/router/LinkRouter.ts
@@ -1,18 +1,58 @@
 import express, {Request, Response} from 'express'
+import { isUrlValid } from '../utils/validUrl'
+import { nanoid } from 'nanoid'
+const config = require('config')
+const LinkSchema = require('./models/LinkSchema')
+
 
 const router = express.Router()
 
-const baseLinkModel = ""
-
-const shortenLink = (req: Request, res:Response) => {
+const shortenLink = async (req: Request, res:Response) => {
     const longLink = req.body.longLink
     const link: string = req.body.link
-    let forwardTo;
-    let newLink;
+    const baseUrl = config.get('baseUrl');
 
-    
+    // chck if long url is valid
+    if (!isUrlValid(link)) {
+        return res.status(400).json({message: "Boss man, your url isn't valid. Abeg no spoil our name"})
+    }
 
-    res.send('Shortened link')
+
+    if (isUrlValid(longLink)) {
+        try {
+            let url = LinkSchema.findOne({ longLink });
+
+            if (url) {
+                console.log("Already exists....");
+                return res.status(201).json({ data: url });
+            } else {
+                //  create url code
+                let urlCode = nanoid(10);
+                let shortLink = baseUrl + '/' + urlCode;
+
+                url = new LinkSchema({
+                    longLink,
+                    shortLink,
+                    urlCode,
+                    date: new Date()
+                })
+
+                console.log("Saving new record.....")
+
+                await url.save();
+                return res.status(201).json({ data: url })
+            }
+        
+        } 
+        
+        catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Sonme error occured" })
+        }    
+    } else {
+        return res.status(400).json({ message: "invalid long url" })
+
+    }
 }
 
 
