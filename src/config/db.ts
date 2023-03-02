4339cdf950625f9327dcde3dcf241c1f1e4735b2
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-03-02 13:50:16 +0100

    adding database

---
+++ src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const config = require('config');
+
+const db = config.get('mongodbUrl')
+
+const connectDb = async () => {
+
+    try {
+        await mongoose.connect(db, {
+            // useNewUrlParser: true
+        })
+        console.log('Mongodb Connected....');
+    } catch (error: any) {
+        console.error("Cannot connect to MongoDB. Existing appliction startup")
+        console.error(error.message)
+        process.exit(-1)
+    }
+}
+
+
+module.exports = connectDb;
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,12 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use(express.json())
 
+// routes
 app.use('/', require('./router/LinkRouter'))
+
+// app running 
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`)
+})
